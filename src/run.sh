cb8fe287b7e35f788f7f537e5f9dec6ce4776dad
gavjan <gavjan@github.com>
2020-09-18 15:50:17 +0200

    sh changes

--- src/run.sh
+++ src/run.sh
@@ -7,13 +7,21 @@ function ctrl_c() {
 	rm result.xml 2> /dev/null
 }
 
-if [ ! -d input ]; then
-  read -p "[ERROR] input folder is missing. Press enter to exit"
+if [ ! -d input ] ; then
+  mkdir input
+  read -p "[ERROR] input folder is empty. Press enter to exit"
   exit 1
 fi
+
+if [ ! -f input/todo.html ] ; then
+  read -p "[ERROR] input folder is empty. Press enter to exit"
+  exit 1
+fi
+
 if [ ! -d output ]; then
   mkdir output
 fi
+
 rm output/* 2> /dev/null
 
 cat input/todo.html | tr -d '\n' | grep -o ">todo</H3>.*" | cut -c 11- > todo.html
