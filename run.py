a3d486b55dfe0f33135a1966caa6682bfe088869
gavjan <gavjan@github.com>
2021-03-16 01:08:54 +0100

    del fix

--- run.py
+++ run.py
@@ -285,6 +285,7 @@ def process_prods(db):
     async_get(jobs, parse_prod)
     for x in to_delete:
         if x in db:
+            del db['img_hash'][db[x]['img_hash']]
             del db[x]
 
 
