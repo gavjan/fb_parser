e2afc098cc5a3f39c17915fb9994a7d4684f8e19
gavjan <gevochobanyan@gmail.com>
2022-01-05 20:52:35 +0100

    -

--- run.py
+++ run.py
@@ -145,7 +145,7 @@ def item_to_xml(_json):
         del copy["size"]
     else:
         copy["size"] = arr_to_string(copy["size"])
-        copy['description'] += f"\n\nՉափսեր {copy['size']}"
+        copy['description'] += f"\n\n- Չափսեր {copy['size']}"
 
     if copy["price"] == "0 AMD":
         copy["price"] = copy["sale_price"]
